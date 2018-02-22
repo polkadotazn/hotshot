e9d42d074bf6cbe375ee0c9277a0ff605af3efb9
Emmanuel <emmanuel.feygelson@gmail.com>
2018-02-22 10:19:31 -0800

    Google Maps Search and current location

--- HotShot/AppDelegate.swift
+++ HotShot/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         GMSServices.provideAPIKey(GoogleMapsKey123)
+        GMSPlacesClient.provideAPIKey(GoogleMapsKey123)
         FirebaseApp.configure()
         return true
     }
